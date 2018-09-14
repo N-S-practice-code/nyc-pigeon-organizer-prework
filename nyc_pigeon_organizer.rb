7be93e3f5b0ec31613bda54c0544a7c567469e3d
Nico-ehs <nico.ehs05@gmail.com>
2018-09-14 18:20:57 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,7 +4,9 @@ def nyc_pigeon_organizer(data)
   data.values.flatten.values.each do |el|
     pigeon_list[el]={}
     data.each do |k1,v1|
-      pigeon_list[el][k1]
+      v1.select{|k2,v2| }
+      pigeon_list[el][k1]=[]
+      
     end
     
   end

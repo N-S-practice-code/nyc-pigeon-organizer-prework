79415b34d0c63a8257c51c3f18573adc1d5481d0
Nico-ehs <nico.ehs05@gmail.com>
2018-09-14 18:29:34 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -7,7 +7,7 @@ def nyc_pigeon_organizer(data)
   data.values.flatten.map{|h| h.values}.flatten.uniq.each do |el1|
     pigeon_list[el1]={}
     data.each do |k1,v1|
-      pigeon_list[el1][k1]=[v1.select{|k2,v2| v2.include?(el1)}.keys.map{|el2| el2.to_s}]
+      pigeon_list[el1][k1]=[v1.select{|k2,v2| v2.include?(el1)}.keys.map{|el2| el2.to_s}].flatten
       binding.pry
     end
   end
